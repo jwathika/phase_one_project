0be72eef32c7bc76671544c3e633ea9d8bef23d2
jwathika <wathika02@gmail.com>
2024-04-12 10:32:33 +0300

    UI

--- public/js/index.js
+++ public/js/index.js
@@ -22,6 +22,7 @@ window.onload = () => {
 						.map((lang) => lang.name)
 						.join(', ');
 					let row = `<tr>
+					<td>${newData.rank}</td>
 					<td>
 					<div style="display: flex; align-items: center;">
                     <img src="${newData.user.photo}" alt="User Photo" style="width: 50px; height: 50px; margin-right: 10px;">
